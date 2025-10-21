5fc4e77fcf0e7ba804d4f20a4fb378ac4a1cd82d
Mintia877 <corgi877@gmail.com>
2025-10-21 19:49:26 +0900

    コミット5回目　課題提出1

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,5 +1,7 @@
 package raisetech.StudentManagement;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,8 @@ public class StudentManagementApplication {
 
   private String name = "Enami Kouji";
   private String age = "37";
+
+  private Map<String, String> studentMap = new HashMap<>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
@@ -28,13 +32,16 @@ public class StudentManagementApplication {
     this.age = age;
   }
 
+  @PostMapping("/StudentMap")
+  public void setStudentMap (String name, String age) {
+    studentMap.put(name, age);
+    this.name = name;
+    this.age = age;
+  }
+
   @PostMapping("/studentName")
   public void updateStudentName(String name) {
     this.name = name;
-  }
 
-  @PostMapping("/studentAge")
-  public void updateStudentAge(String age) {
-    this.age = age;
   }
 }
